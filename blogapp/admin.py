bd0866aeba5bf5d60240b7a4bc3e423ef62de541
Arian Amirpour <ariyan.amirpour@gmail.com>
2025-05-22 22:59:59 +0330

    setup admin panel

--- blogapp/admin.py
+++ blogapp/admin.py
@@ -1,3 +1,10 @@
 from django.contrib import admin
-
+from blogapp.models import Post
 # Register your models here.
+
+class PostAdmin(admin.ModelAdmin):
+    date_hierarchy = 'created_date'
+    empty_value_display = '-'
+    list_display = ('title', 'counted_views' , 'created_date', 'updated_date', 'status')
+
+admin.site.register(Post, PostAdmin)
